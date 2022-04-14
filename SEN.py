de13ee41bbe656d5ecf0e2b214fb56f35b050d00
mrz <mr.z004@yahoo.com>
2022-04-14 09:36:31 +0530

    made SEN.py functional

--- SEN.py
+++ SEN.py
@@ -1,7 +1,7 @@
 from tkinter import *
+from tkinter import Canvas
 from tkinter.filedialog import askopenfilename, asksaveasfilename
-from PIL import Image, ImageTk
-
+from PIL import Image, ImageTk, ImageOps
 
 myWidth = 1080
 myHeight = 720
@@ -16,23 +16,27 @@ root.config(bg='#222')
 menuBar = Menu(root)
 
 ##### Added canvas #####
-canva = Canvas(root, width=myWidth-20, height=myHeight-20, bg='#ccc').pack(pady=40)
-canva.create_image(10, 10, anchor=NW, image=PhotoImage('D://SY_Projects//logo.png'))
+canvas = Canvas(root, width=myWidth - 20, height=myHeight - 20, bg='#ccc')
+canvas.pack(pady=40)
+imgRef = PhotoImage(file="D:\SY_Projects\logo.png")
+# canvas.create_image(10, 10, anchor=NW, image=imgRef)
 
 img = None
 
-def openImg():
+
+def openImg(imgpath=True):
     global img
-    global canva
-    imgPath = askopenfilename(defaultextension=".png", filetypes=[("png", "*.png"),
-                                                                ("jpg", "*.jpg"),
-                                                                ("jpeg", "*.jpeg"),
-                                                                ("All files", "*.*"), ])
-    img = Image.open(imgPath)
-    photo = ImageTk.PhotoImage(img)
-    canva.create_image(10, 10, anchor=NW, image=photo)
+    global imgRef
+    if imgpath:
+        imgPath = askopenfilename(defaultextension=".png", filetypes=[("png", "*.png"),
+                                                                      ("jpg", "*.jpg"),
+                                                                      ("jpeg", "*.jpeg"),
+                                                                      ("All files", "*.*"), ])
+        img = Image.open(imgPath)
+        print(imgPath)
+    imgRef = ImageTk.PhotoImage(img)
+    canvas.create_image(10, 10, anchor=NW, image=imgRef)
     root.update()
-    print(imgPath)
 
 
 def saveImg():
@@ -48,31 +52,54 @@ def refresh():
 
 
 def rotate90Clockwise():
-    global imgObj, photo
-    print(imgObj)
-    imgObj =imgObj.rotate(-90)
-    
-    imgObj.show()
+    global img
+    # print(img)
+    img = img.rotate(-90, expand=True)
+    openImg(imgpath=False)
 
 
 def rotate90AntiClockwise():
-    pass
+    global img
+    img = img.rotate(90, expand=True)
+    openImg(imgpath=False)
 
 
 def rotateCustom():
-    pass
+
+    def apply():
+        global img
+        # print(rotationAngle.get())
+        img = img.rotate(int(rotationAngle.get())*-1, expand=True)
+        openImg(imgpath=False)
+        rotatBox.destroy()
+
+    rotatBox = Tk()
+    msg = Label(rotatBox, text="Enter rotation angle").pack(padx=20, pady=10)
+    rotationAngle = Entry(rotatBox)
+    rotationAngle.pack(padx=20, pady=10)
+    submit = Button(rotatBox, text="Apply", command=apply).pack(padx=20, pady=5)
+    rotatBox.mainloop()
 
 
 def resizeImg():
     pass
 
 
-def reflectImg():
-    pass
+def reflectImgVertical():
+    global img
+    img = ImageOps.flip(img)
+    openImg(imgpath=False)
+
+
+def reflectImgHorizontal():
+    global img
+    img = ImageOps.mirror(img)
+    openImg(imgpath=False)
 
 
 def cropImg():
     pass
+
 
 ##### Added fileMenu #####
 fileMenu = Menu(root, tearoff=0, font=("Times", 12))
@@ -85,12 +112,13 @@ menuBar.add_cascade(label="File", menu=fileMenu)
 
 ##### Added EditMenu #####
 editMenu = Menu(root, tearoff=0, font=("Times", 12))
-editMenu.add_command(label="Rotate 90", command=rotate90Clockwise)
-editMenu.add_command(label="Rotate 90", command=rotate90AntiClockwise)
+editMenu.add_command(label="Rotate Clockwise 90", command=rotate90Clockwise)
+editMenu.add_command(label="Rotate Anti-Clockwise 90", command=rotate90AntiClockwise)
 editMenu.add_command(label="Rotate", command=rotateCustom)
 editMenu.add_separator()
 editMenu.add_command(label="Resize", command=resizeImg)
-editMenu.add_command(label="Reflect", command=reflectImg)
+editMenu.add_command(label="Reflect Vertically", command=reflectImgVertical)
+editMenu.add_command(label="Reflect Horizontally", command=reflectImgHorizontal)
 editMenu.add_command(label="Crop", command=cropImg)
 menuBar.add_cascade(label="Edit", menu=editMenu)
 
